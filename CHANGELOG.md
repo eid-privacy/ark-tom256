7ae352eea5413775f1acdd7f9cb47abc99f8fa1b
Linus Gasser <linus.gasser@epfl.ch>
2026-03-17 14:25:03 +0100

    Fixing wrong values in README.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# 0.1.4 - 2026-03-17
+- Updated values of the order and field size in the documentation
+
 # 0.1.3 - 2026-03-17
 - Initial version
--- src/fields/fr.rs
+++ src/fields/fr.rs
@@ -1,5 +1,7 @@
 use ark_ff::fields::{Fp256, MontBackend, MontConfig};
 
+// This corresponds to the modulo of the
+// github.com:arkworks-rs/algebra/curves/secp256r1/src/fields/fq.rs
 #[derive(MontConfig)]
 #[modulus = "115792089210356248762697446949407573530086143415290314195533631308867097853951"]
 #[generator = "6"]
